541d6e9a354f31dcaffcc48b69bf05aab2a5ad3c
ABU TALHA <talhaqc@gmail.com>
2016-05-13 20:10:28 +0600

    prothom alo crawler

--- project_details/app/Http/routes.php
+++ project_details/app/Http/routes.php
@@ -12,13 +12,9 @@
 */
 
 Route::get('/', function () {
-	return Redirect::route('dashboard');
+	//return Redirect::route('dashboard');
+	return View::make('welcome');
 });
-// Route::get('/',function(){
-// 	// return \App\User::first();
-// 	//return array_keys(config('customConfig.roles'));
-// 	return redirect()->route('login');
-// });
 
 
 
@@ -51,58 +47,62 @@ Route::group(array('middleware' => 'auth'), function()
 });
 
 
+Route::get('prothomAlo', ['uses' => 'CrawlerController@prothomAlo']);
+Route::get('test', ['uses' => 'CrawlerController@test']);
 
 
 
-Route::get('profile1',function(){
-	return View::make('template.profile')->with('title','Profile');
-});
 
-Route::get('timeline',function(){
-	return View::make('template.timeline')->with('title','Timeline');
-});
-
-Route::get('widgets',function(){
-	return View::make('template.widgets')->with('title','Widgets');
-});
-
-Route::get('portlets',function(){
-	return View::make('template.portlets')->with('title','Portlets');
-});
-
-Route::get('panel',function(){
-	return View::make('template.panel')->with('title','Panel');
-});
-
-Route::get('chart_x',function(){
-	return View::make('template.chart_x')->with('title','Chart_x');
-});
-
-
-Route::get('index2',function(){
-	return View::make('template.dashboard')->with('title','Dashboard');
-});
-
-Route::get('gmap',function(){
-	return View::make('template.gmap')->with('title','GMap');
-});
-
-Route::get('friends',function(){
-	return View::make('template.friends')->with('title','Friends');
-});
-
-Route::get('adForm',function(){
-	return View::make('template.advanced_form')->with('title','Advanced Form');//problem
-});
-
-Route::get('form-wizard',function(){
-	return View::make('template.form_wizard')->with('title','Form Wizard');
-});
-
-Route::get('dataTable',function(){
-	return View::make('template.datatable')->with('title','Data Table');
-});
-
+//
+//Route::get('profile1',function(){
+//	return View::make('template.profile')->with('title','Profile');
+//});
+//
+//Route::get('timeline',function(){
+//	return View::make('template.timeline')->with('title','Timeline');
+//});
+//
+//Route::get('widgets',function(){
+//	return View::make('template.widgets')->with('title','Widgets');
+//});
+//
+//Route::get('portlets',function(){
+//	return View::make('template.portlets')->with('title','Portlets');
+//});
+//
+//Route::get('panel',function(){
+//	return View::make('template.panel')->with('title','Panel');
+//});
+//
+//Route::get('chart_x',function(){
+//	return View::make('template.chart_x')->with('title','Chart_x');
+//});
+//
+//
+//Route::get('index2',function(){
+//	return View::make('template.dashboard')->with('title','Dashboard');
+//});
+//
+//Route::get('gmap',function(){
+//	return View::make('template.gmap')->with('title','GMap');
+//});
+//
+//Route::get('friends',function(){
+//	return View::make('template.friends')->with('title','Friends');
+//});
+//
+//Route::get('adForm',function(){
+//	return View::make('template.advanced_form')->with('title','Advanced Form');//problem
+//});
+//
+//Route::get('form-wizard',function(){
+//	return View::make('template.form_wizard')->with('title','Form Wizard');
+//});
+//
+//Route::get('dataTable',function(){
+//	return View::make('template.datatable')->with('title','Data Table');
+//});
+//
 
 
 
